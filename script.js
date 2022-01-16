e2fca76eb8ff31c14405a2f7fc133abbf14fb01d
Tahlia La Galia <tahlia.lagalia@gmail.com>
2022-01-16 17:47:17 +0800

    Added html and css for game page

---
+++ script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector("#question");
+const choices = document.querySelector("#question");
+const question = document.querySelector("#question");
+const question = document.querySelector("#question");
+const question = document.querySelector("#question");
